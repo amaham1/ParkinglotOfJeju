5a5dd60221190f7e2e32dba1bb27e39249e87fe3
amaham1 <37212080+amaham1@users.noreply.github.com>
2020-06-17 16:50:29 +0900

    Create AppinfoActivity.kt

---
+++ app/src/main/java/com/jursi/parkinglotofjeju/AppinfoActivity.kt
@@ -0,0 +1,13 @@
+package com.jursi.parkinglotofjeju
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+
+class AppinfoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_appinfo)
+    }
+}
