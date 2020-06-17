bae3ba43d8c45ea210570e666083f21e68b17906
amaham1 <37212080+amaham1@users.noreply.github.com>
2020-06-17 16:50:33 +0900

    Update FragmentOfParkingLotList.kt

--- app/src/main/java/com/jursi/parkinglotofjeju/FragmentOfParkingLotList.kt
+++ app/src/main/java/com/jursi/parkinglotofjeju/FragmentOfParkingLotList.kt
@@ -1,38 +1,64 @@
 package com.jursi.parkinglotofjeju
 
-import android.app.ProgressDialog
-import android.os.AsyncTask
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
+import android.view.*
+import android.view.View.GONE
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.activity_of_parking_lot_list.*
+import com.jursi.parkinglotofjeju.data.ParkinglotInfoItem
+import com.jursi.parkinglotofjeju.viewmodel.MainViewModel
 import kotlinx.android.synthetic.main.fragment_of_parking_lot_list.*
-import org.xmlpull.v1.XmlPullParser
-import org.xmlpull.v1.XmlPullParserFactory
-import splitties.toast.toast
-import java.io.InputStreamReader
-import java.net.URL
-
-
-/*var parkinglotInfoArrayList: ArrayList<ParkinglotInfo>? = null // ParkinglotInfo 데이터 클래스 리스트 참조용 변수
-var classOfParkinglotInfo: ParkinglotInfo? = null  //ParkinglotInfo 데이터 클래스 참조용 변수
-var requestUrl: String? = null //API주소 참조용 변수
-var aTpm: FragmentOfParkingLotList.ParkinglotParser? = null //ParkinglotParser Asycn 참조용 변수
-var parserAdapter: recyclerViewAdapter? = null //RecyclerViewAdapter 클래스 참조용 변수*/
+import kotlin.collections.ArrayList
+import androidx.lifecycle.Observer
 
 
 class FragmentOfParkingLotList : Fragment() {
+    private val mParking = ArrayList<ParkinglotInfoItem>()
+    private var mRecyclerView: RecyclerView? = null
+    private val mParkingAdapter = recyclerViewAdapter(mParking)
+    private var parkingViewModel: MainViewModel? = null
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val rootView = inflater.inflate(R.layout.fragment_of_parking_lot_list, container, false)
+        //리사이클러뷰 지정
+        mRecyclerView = rootView.findViewById(R.id.recyclerViewOfMain)
         return rootView
+    } //onCreateView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        shimmer_view_container.startShimmerAnimation()
+
+        val context = view.context
+        mRecyclerView!!.layoutManager = LinearLayoutManager(context)
+        mRecyclerView!!.adapter = mParkingAdapter
+
     }
 
+    //리사이클러뷰로 보내기
+    fun setParkinglot(parking: List<ParkinglotInfoItem>) {
+        mParking.clear()
+        mParkingAdapter.notifyDataSetChanged()
+        for (i in parking) {
+            if (!mParking.contains(i)) {
+                mParking.add(i)
+                mParkingAdapter.notifyItemInserted(mParking.indexOf(i))
+            }
+        }
+    }
 
-
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        parkingViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        parkingViewModel?.data?.observe(viewLifecycleOwner, Observer { it ->
+            it?.let {
+                setParkinglot(it)
+                shimmer_view_container.stopShimmerAnimation()
+                shimmer_view_container.visibility = GONE
+            }
+        })
+    }
 }
+
